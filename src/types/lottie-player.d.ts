025e985d72634efcf64408dbd34669350a72aa5b
nimeduhansaka <nimedu.h.gbro@gmail.com>
2025-11-16 16:02:46 +0530

    Fixed issues

--- src/types/lottie-player.d.ts
+++ src/types/lottie-player.d.ts
@@ -1,25 +1,25 @@
 
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      'lottie-player': React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement>,
-        HTMLElement
-      > & {
-        src?: string;
-        background?: string;
-        speed?: string | number;
-        loop?: boolean | '';
-        autoplay?: boolean | '';
-        mode?: string;
-        renderer?: string;
-        controls?: boolean | '';
-        style?: React.CSSProperties;
-        'aria-label'?: string;
-      };
-    }
-  }
-}
+// declare global {
+//   namespace JSX {
+//     interface IntrinsicElements {
+//       'lottie-player': React.DetailedHTMLProps<
+//         React.HTMLAttributes<HTMLElement>,
+//         HTMLElement
+//       > & {
+//         src?: string;
+//         background?: string;
+//         speed?: string | number;
+//         loop?: boolean | '';
+//         autoplay?: boolean | '';
+//         mode?: string;
+//         renderer?: string;
+//         controls?: boolean | '';
+//         style?: React.CSSProperties;
+//         'aria-label'?: string;
+//       };
+//     }
+//   }
+// }
 
-export {};
+// export {};
